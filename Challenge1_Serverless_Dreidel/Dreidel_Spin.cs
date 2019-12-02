5a2f1b970f744a929e15b9448d4e8a678e06a3d4
krunalsolanki <krunalsolanki@gmail.com>
2019-12-03 00:15:02 +0530

    Add project files.

---
+++ Challenge1_Serverless_Dreidel/Dreidel_Spin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Challenge1_Serverless_Dreidel
+{
+    public static class Dreidel_Spin
+    {
+        [FunctionName("Dreidel_Spin")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            List<KeyValuePair<string, object>> dreidel = new List<KeyValuePair<string, object>>();
+
+            dreidel.Add(new KeyValuePair<string, object>("Nun", "נ"));
+            dreidel.Add(new KeyValuePair<string, object>("Gimmel", "ג"));
+            dreidel.Add(new KeyValuePair<string, object>("Hay", "ה"));
+            dreidel.Add(new KeyValuePair<string, object>("Shin", "ש"));
+
+            return (ActionResult)new OkObjectResult(dreidel[new Random().Next(dreidel.Count)]);
+        }
+    }
+}
